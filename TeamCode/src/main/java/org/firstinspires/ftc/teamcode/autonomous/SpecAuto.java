6b969b5b37ff327acf592e7c024b4d08a56c7522
Brüno <a01667312@tec.mx>
2025-08-25 14:15:34 -0600

    Create SpecAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SpecAuto.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+import androidx.annotation.NonNull;
+
+// RR libs
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.acmerobotics.roadrunner.ParallelAction;
+
+// Non RR libs
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Claw;
+
+@Config
+@Autonomous (name = "1 Spec Auto")
+public class SpecAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //TODO Set estimated position
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        Claw claw = new Claw(hardwareMap,telemetry);
+
+        Action tab1 = drive.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2)
+                .build();
+    }
+}
